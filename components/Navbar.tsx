e2cddf22758cb32bd98f85a0febb03a1f4e1e982
tj <tharunkumar200230@gmail.com>
2023-07-12 23:18:16 +0530

    basic web structure

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween">
+        <div className="flex-1 flexStart gap-10">
+            <Link href='/'>
+                <Image src='/logo.svg'
+                width={115}
+                height={43}
+                alt='flexxible'
+                />
+            </Link>
+            <ul className="xl:flex hidden text-small gap-7">
+                
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
